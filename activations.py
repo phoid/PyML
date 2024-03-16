42089adbf49c69a96af001c6d33a0329653d854c
Patrick Reynolds <patrey15@gmail.com>
2024-03-15 20:13:57 -0700

    Implemented relu

--- activations.py
+++ activations.py
@@ -1,13 +1,26 @@
 import numpy as np
 
 
-def relu():
-    pass
+def relu(x):
+    """
+    Applies ReLU to a matrix:
+
+    """
+    return np.maximum(x, 0)
 
 
 def sigmoid():
+    """
+    Applies Sigmoid to a matrix:
+
+    """
     pass
 
 
 def softmax():
+    """
+    Applies softmax to a matrix:
+
+
+    """
     pass
