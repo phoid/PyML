752ffc394d66577e77f7f31292163073ccd1faa2
Patrick Reynolds <patrey15@gmail.com>
2024-03-15 20:13:43 -0700

    Implemented Linear + Weight Init
    Implemented the forward pass for Linear Class as well as a uniformly distributed weight initialization

--- layers.py
+++ layers.py
@@ -1,16 +1,32 @@
 import numpy as np
 
-class Linear():
 
-    def __init__(self):
+class Linear:
+    """
+    defines the weights matrix for an individual linear layer
+    """
+
+    def __init__(self, nIn, nOut):
+        self.weights = np.random.rand(nOut, nIn) - 0.5
+
+    def backprop(self):
         pass
 
-    def forward(self):
-        pass
+    def forward(self, X):
+        """Apply the linear Combination"""
+        return X.T * self.weights
 
-class layer():
+
+class Conv2d:
+    """
+    define an m * n weights matrix (Kernel)
+    """
 
     def __init__(self):
         pass
 
     def backprop():
+        pass
+
+    def forward():
+        pass
