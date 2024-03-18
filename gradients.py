cd8a51d30a48d614705aa384221a0e544cdcc34f
Patrick Reynolds <patrey15@gmail.com>
2024-03-17 19:14:15 -0700

    Implemented Grad Clipping
    Implemented gradient clipping through standard norm clopping

--- gradients.py
+++ gradients.py
@@ -1,11 +1,22 @@
 import numpy as np
 
 
-def stochastic(input, cost, weights=None, lr=0.001):
+def stochastic(input, cost, weights=None, lr=0.001, clip=True, norm=1.0):
     """
     Calculates a Stocastic approximation of the Gradient
     """
     if weights is not None:
-        return lr * input.dot(np.array([weights]).T) * cost
+        grad = lr * input.dot(np.array([weights]).T) * cost
     else:
-        return lr * input * cost
+        grad = lr * input * cost
+    if clip:
+        return clip_by_norm(grad, max_norm=norm)
+    return grad
+
+
+def clip_by_norm(gradients, max_norm=1.0):
+    norm = np.linalg.norm(gradients)
+    if norm > max_norm:
+        clipping_factor = max_norm / norm
+        gradients = gradients * clipping_factor
+    return gradients
