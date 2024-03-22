40c2d86a3d5e630c77a53313a3ae88af15fe196f
Patrick Reynolds <patrey15@gmail.com>
2024-03-21 19:00:32 -0700

    Implementing Batchnorm1d

--- regularizers.py
+++ regularizers.py
@@ -5,5 +5,16 @@ def dropout():
     pass
 
 
-def batchnorm1d(batch):
-    return
+class BatchNorm1D:
+    def __init__(self, num_features, momentum=0.9, eps=1e-5):
+        self.num_features = num_features
+        self.momentum = momentum
+        self.eps = eps
+
+        # Initialize parameters (gamma and beta)
+        self.gamma = np.ones(num_features)
+        self.beta = np.zeros(num_features)
+
+        # Moving averages used during inference
+        self.running_mean = np.zeros(num_features)
+        self.running_var = np.ones(num_features)
